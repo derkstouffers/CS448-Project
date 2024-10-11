6410b8f558aa8f316545de6f44e3d22a87e03785
derkstouffers <dsha77er@gmail.com>
2024-10-10 19:49:41 -0600

    add load screen and fix camera

--- inventory/Global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var playing = false
-var TileID = 0
-var current_tile_coords = Vector2i(0,0)
-var place_tile = false
-var backgroundLayer = false
-var playerLayer = false
-var foregroundLayer = false
